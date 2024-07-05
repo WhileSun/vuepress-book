9cc28d20afad407633465c0b4a5e63bff729ae6b
whilesun <1207460050@qq.com>
2024-07-05 23:04:31 +0800

    podman md add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,5 +4,46 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  title: '技术文档',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '前端',
+        prefix: '/frontend/',
+        children: [
+          {
+            text: '小程序',
+            // prefix: 'miniprogram/',
+            link: 'miniprogram/'
+          }
+        ]
+      },
+      {
+        text:'运维',
+        prefix: '/devops/',
+        children: [
+          {
+            text: 'podman',
+            // prefix: 'podman/',
+            link: 'podman/'
+          }
+        ]
+      }
+    ],
+    sidebar:
+    {
+      '/frontend/': [
+        {
+          // text: '微信小程序',
+          // collapsible: true,
+          // 前缀可以是相对路径，等同于 `prefix: /reference/bundler/`
+          prefix: 'miniprogram/',
+          children: ['index', 'config'],
+        },
+      ],
+      // '/devops/':'heading',
+      '/':'heading'
+    },
+    sidebarDepth:3
+  }),
 })
---
+++ docs/devops/podman/index.md
@@ -0,0 +1,59 @@
+# podman
+
+## 安装
+
+### 官网文档
+[podman官网安装文档](https://podman.io/docs/installation)  
+
+### linux ubuntu安装
+``` bash 
+# Ubuntu 20.10 and newer
+sudo apt-get update
+sudo apt-get -y install podman
+```
+
+
+## 配置
+
+### 镜像加速
+
+#### 镜像源
+目前国内还可以使用的[镜像源](https://www.jianshu.com/p/ce25edea73f5)
+
+#### 打开配置文件
+``` bash
+sudo vim /etc/containers/registries.conf
+```
+
+#### 修改配置
+**修改后记得重启podman**
+``` bash
+# 取消从默认地址搜索的仓库域名
+unqualified-search-registries = ["docker.io"]
+
+# 自定义搜索器
+[[registry]]
+# 仓库前缀（镜像前缀）
+prefix = "docker.io"
+# 加速器地址，此处配置的 docker 中国区源
+location = "dockerpull.com"
+# 允许通过 http 协议获取镜像
+insecure = true
+```
+
+## 部署nginx
+
+### 拉取nginx
+``` bash
+podman pull nginx:alpine3.19
+```
+
+### 进入nginx容器查看目录
+``` bash
+# 进入容器 --rm 表示退出后会删除容器
+podman run -it --rm nginx:alpine3.19 /bin/sh
+# nginx配置目录所在位置
+/etc/nginx
+
+```
+
